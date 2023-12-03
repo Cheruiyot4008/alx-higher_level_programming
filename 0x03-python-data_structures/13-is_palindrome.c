a5666471ee7cefe2b95e9c7b9730f0e12549386c
cheruiyot4008 <cheruiyotbrian454@gmail.com>
2023-12-02 22:10:27 -0800

    fourteenth task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,87 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 
-/**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
+typedef struct listint {
+    int n;
+    struct listint *next;
+} listint_t;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+int is_palindrome(listint_t **head) {
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+
+    listint_t *slow = *head, *fast = *head, *prev_slow = *head, *second_half;
+    int is_palindrome = 1;
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+
+        listint_t *next = slow->next;
+        slow->next = prev_slow;
+        prev_slow = slow;
+        slow = next;
+    }
+
+    if (fast != NULL)
+        slow = slow->next;
+
+    second_half = slow;
+    while (prev_slow != NULL && second_half != NULL) {
+        if (prev_slow->n != second_half->n) {
+            is_palindrome = 0;
+            break;
+        }
+        prev_slow = prev_slow->next;
+        second_half = second_half->next;
+    }
+
+    slow = NULL;
+    while (prev_slow != NULL) {
+        listint_t *next = prev_slow->next;
+        prev_slow->next = slow;
+        slow = prev_slow;
+        prev_slow = next;
+    }
+    *head = slow;
+
+    return is_palindrome;
 }
-/**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
-int is_palindrome(listint_t **head)
-{
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+int main(void) {
+    listint_t *head = NULL;
+
+    for (int i = 1; i <= 3; i++) {
+        listint_t *new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL) {
+            perror("Memory allocation error");
+            return EXIT_FAILURE;
+        }
+        new_node->n = i;
+        new_node->next = head;
+        head = new_node;
+    }
+    for (int i = 3; i >= 1; i--) {
+        listint_t *new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL) {
+            perror("Memory allocation error");
+            return EXIT_FAILURE;
+        }
+        new_node->n = i;
+        new_node->next = head;
+        head = new_node;
+    }
+
+    if (is_palindrome(&head))
+        printf("The list is a palindrome\n");
+    else
+        printf("The list is not a palindrome\n");
+
+    listint_t *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return EXIT_SUCCESS;
 }
